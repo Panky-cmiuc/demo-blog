90bee38368a0863a7b1b28f1fc75d6b31936cdb6
Pankaj Boruah <pankaj.boruah@upgrad.com>
2019-11-29 16:43:49 +0530

    resolve indentation

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,21 @@
 </template>
 
 <script>
-import addBlog from './components/addBlog'
+import addBlog from "./components/addBlog";
 export default {
-
-  name: 'app',
+  name: "app",
   components: {
-    'add-blog':addBlog
+    "add-blog": addBlog
   }
-}
+};
 </script>
 
 <style>
 body {
-margin:0;
+  margin: 0;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
